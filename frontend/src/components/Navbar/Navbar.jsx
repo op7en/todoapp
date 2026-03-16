9b32cc455dd04f965457f77c72f190ff4be21436
Oleg <oleg0kofficial@gmail.com>
2026-03-16 09:25:34 +0300

    Добавил темную тему для header

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Navbar.css";
 import { GiWhiteBook } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { authActions } from "../../store";
+import { BsMoon, BsSun } from "react-icons/bs";
+
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => state.isLoggedIn);
-  console.log(isLoggedIn);
   const dispatch = useDispatch();
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
+
   const logout = () => {
     sessionStorage.clear("id");
     dispatch(authActions.logout());
   };
+
   return (
     <div>
-      <nav className="navbar navbar-expand-lg ">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
             <b>
@@ -34,64 +45,50 @@ const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
               <li className="nav-item mx-2">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link className="nav-link" to="/">
                   Меню
                 </Link>
               </li>
               <li className="nav-item mx-2">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/about"
-                >
+                <Link className="nav-link" to="/about">
                   О нас
                 </Link>
               </li>
               <li className="nav-item mx-2">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/todo"
-                >
+                <Link className="nav-link" to="/todo">
                   ToDo
                 </Link>
+              </li>
+              <li className="nav-item mx-2">
+                <button
+                  className="theme-toggle"
+                  onClick={() => setDarkMode(!darkMode)}
+                >
+                  {darkMode ? <BsSun /> : <BsMoon />}
+                </button>
               </li>
               {!isLoggedIn && (
                 <>
                   <li className="nav-item mx-2">
-                    <Link
-                      className="nav-link active btn-nav"
-                      aria-current="page"
-                      to="/signup"
-                    >
+                    <Link className="nav-link btn-nav" to="/signup">
                       Регистрация
                     </Link>
                   </li>
                   <li className="nav-item mx-2">
-                    <Link
-                      className="nav-link active btn-nav"
-                      aria-current="page"
-                      to="/signin"
-                    >
+                    <Link className="nav-link btn-nav" to="/signin">
                       Войти
                     </Link>
                   </li>
                 </>
               )}
               {isLoggedIn && (
-                <>
-                  <li className="nav-item mx-2" onClick={logout}>
-                    <Link
-                      className="nav-link active btn-nav"
-                      aria-current="page"
-                      to="#"
-                    >
-                      Выйти
-                    </Link>
-                  </li>
-                </>
+                <li className="nav-item mx-2" onClick={logout}>
+                  <Link className="nav-link btn-nav" to="#">
+                    Выйти
+                  </Link>
+                </li>
               )}
             </ul>
           </div>
