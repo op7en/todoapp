56fea05401dfd44eccc6dc52383b6b61240ae86e
Oleg <oleg0kofficial@gmail.com>
2026-03-16 17:18:36 +0300

    -

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const conn = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // ✅ change MONGO_URL to MONGO_URI
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log("Disconnected from MongoDB", err);
